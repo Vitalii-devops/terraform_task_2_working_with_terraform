7e4fda6ef7f795096cd05c21175c1503e41ed3cf
Vitalii <vitalyoirshko@gmail.com>
2025-10-30 23:41:08 +0200

    solution

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "resource_group_name" {
+    type = string
+    default = "example-resources"
+}
+
+variable "location" {
+    type = string
+    default = "East US"
+}
+
+variable "storage_account_name" {
+    type = string
+    default = "storageaccountname"
+}
+
+variable "container_name" {
+    type = string
+    default = "example-container"
+}
+
+variable "blob_name" {
+    type = string
+    default = "example-blob"
+}
